c4ed90b32180b017ac5174ade0e5240860b537a9
lbjcena <lbjcena@gmail.com>
2024-11-09 23:04:42 +0100

    matching logic

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Card({ content, onClick }) {
+export default function Card({ content, onClick, id }) {
   const [isActive, setIsActive] = useState(false)
 
   const handleClick = () => {
@@ -10,7 +10,7 @@ export default function Card({ content, onClick }) {
     }
   }
   return (
-    <div className="card" onClick={handleClick} data-active={isActive}>{content}</div>
+    <div className="card" onClick={handleClick} data-active={isActive} id={`card-${id}`}>{content}</div>
   )
 }
 
--- components/GameBoard.jsx
+++ components/GameBoard.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+
 import Card from "./Card";
 
 
 export default function GameBoard() {
   const cardArray = []
-  const selectedCards = []
-
+  let selectedCards = []
   // array holding values displayed in the game - amount of cards rendered will always be a double of the amount of values inside
   const cardContentItems = ['1', '2', '3', '4', '5', '6', '7', '8']
   const returnPairs = (arr) => {
@@ -18,11 +17,11 @@ export default function GameBoard() {
   }
   const populateBoard = () => {
     for (let i = 0; i < (cardContentItems.length * 2); i++) {
-      cardArray.push(<Card key={i + 1} content={returnPairs(cardContentItems)[i]} />)
+      cardArray.push(<Card key={i + 1} id={i + 1} content={returnPairs(cardContentItems)[i]} />)
     }
     // randomize order in which elements are in the array, so they don't always render the same
     const shuffleValues = () => {
-      for (let i = cardArray.length - 1; i >= 0; i--) {
+      for (let i = cardArray.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1))
         if (j) {
           [cardArray[i], cardArray[j]] = [cardArray[j], cardArray[i]]
@@ -30,20 +29,51 @@ export default function GameBoard() {
       }
     }
     shuffleValues()
-    console.log(cardArray)
+    // console.log(cardArray)
+  }
+
+  const checkForVictory = () => {
+    let match;
+    // check if two selected
+    // if two selected, check if match
+    // if match, mark green
+    // if not, reset all
+
+    if (selectedCards.length === 2) {
+      console.log(`index 0 : ${selectedCards[0]?.props.content} | index 1 : ${selectedCards[1]?.props.content}`)
+      if (selectedCards[0]?.props.content == selectedCards[1]?.props.content) {
+        match = true
+      }
+      console.log(selectedCards[0]?.props.id)
+      console.log(selectedCards[1]?.props.id)
+      console.log(match)
+    }
+    if (match === true) {
+      const firstElem = document.getElementById(`card-${selectedCards[0]?.props.id}`)
+      const secondElem = document.getElementById(`card-${selectedCards[1]?.props.id}`)
+
+      if (firstElem && secondElem) {
+        console.dir(firstElem)
+        console.dir(secondElem)
+        firstElem.style.background = 'red'
+        secondElem.style.background = 'red'
+        selectedCards = []
+      }
+    }
   }
   const addToSelected = (element) => {
     selectedCards.push(element)
     console.log(selectedCards)
+    return selectedCards
   }
 
 
   populateBoard()
-  console.log(returnPairs(cardContentItems))
+  // console.log(returnPairs(cardContentItems))
   return (
     <section className="game_board">
       {cardArray.map((e) => {
-        return (<Card onClick={() => addToSelected(e.props.content)} key={e.key} content={e.props.content} ></Card >)
+        return (<Card onClick={() => addToSelected(e) && checkForVictory()} key={e?.key} id={e?.props.id} content={e?.props.content}></Card >)
       })}
     </section >
 
